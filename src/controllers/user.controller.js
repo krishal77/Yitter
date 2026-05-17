acfbcbd7aab79265c3a47eb88578e9d8d88aac83
krishal77 <kthapaliya77@gmail.com>
2026-05-17 22:00:51 +0545

    fix: correct user ID reference in changeCurrentPassword and add updateAccountDetails functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -175,7 +175,7 @@ const refreshAccessToken= asyncHandler(async(req,res)=>{
 
 const changeCurrentPassword= asyncHandler(async(req,res)=>{
 const {oldpassword,newPassword}=req.body
-const user=await User.findById(req.user?.id)
+const user=await User.findById(req.user?._id)
 const isPasswordCorrect= await user.isPasswordCorrect(oldpassword)
 if(!isPasswordCorrect){
     throw new ApiError(400,"Invalid old password")
@@ -192,8 +192,19 @@ const getCurrentUser= asyncHandler(async(req,res)=>{
    return res.status(200).json(new ApiResponse(200,req.user,"current user fetched successfully"))
 })
 
-
-
+const updateAccountDetails= asyncHandler(async(req,res)=>{
+    const {fullName,email}= req.body
+    if(!fullName || !email){
+        throw new ApiError(400,"All fields are required")
+    }
+    const user=User.findByIdAndUpdate(req.user?._id,{
+        $set:{
+            fullName,
+            email:email
+        }
+    },{new:true}).select("-passsword")
+return res.status(200).json(new ApiResponse(200,user,"account details updated successfully"))
+})
 export {userRegister,
     loginUser,
      logoutUser,
