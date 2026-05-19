99d32b88288deba950c34ba5086e92893a72ed7a
krishal77 <kthapaliya77@gmail.com>
2026-05-19 21:57:00 +0545

    fix: update route definitions for user-related endpoints in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,8 @@ import { loginUser,
     getCurrentUser,
     updateUserAvatar,
     updateUserCoverImage,
-    getChannelUserProfile} from "../controllers/user.controller.js";
+    getChannelUserProfile,
+    getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -30,11 +31,12 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/changePassword").post(verifyJWT,changeCurrentPassword)
-router.route("/current-user").post(verifyJWT,getCurrentUser)
-router.route("/update-account").post(verifyJWT,updateUserAvatar)
-router.route("/avatar").post(verifyJWT,upload.single("avatar"),updateUserCoverImage)
-router.route("/cover-image").post(verifyJWT, upload.single("coverImage"),getChannelUserProfile)
-
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateUserAvatar)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),getChannelUserProfile)
+router.route("/c/:username").get(verifyJWT,getChannelUserProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router 
