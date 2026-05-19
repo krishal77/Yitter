59129f01be3135bc594723423bda8f57b3b8df5a
krishal77 <kthapaliya77@gmail.com>
2026-05-19 21:52:13 +0545

    feat: add getWatchHistory functionality to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -123,7 +123,7 @@ const logoutUser=asyncHandler(async(req,res)=>{
             }
         },
         {
-            new:true
+            new:true // returns new updated field
         }
     )
 
@@ -304,6 +304,62 @@ return res
 )
 )
 })
+
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+
 export {userRegister,
     loginUser,
      logoutUser,
@@ -312,5 +368,6 @@ export {userRegister,
     getCurrentUser,
     updateUserAvatar,
     updateUserCoverImage,
-    getChannelUserProfile
+    getChannelUserProfile,
+    getWatchHistory
 } 
