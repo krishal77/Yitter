0e7042c1256c5c71d7442d928ea7d6735931771d
krishal77 <kthapaliya77@gmail.com>
2026-05-20 18:54:26 +0545

    fix: clean up import statements and adjust formatting in app.js and auth.middleware.js

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ app.use(cookieParser());
 
 //routes import 
 import userRouter from "./routes/user.routes.js"
-
+import videoRouter from "./routes/video.routes.js"
 //routes decleration
 app.use("/api/v1/users", userRouter)
-
+app.use("/api/v1/video", videoRouter)
 
 export {app};
