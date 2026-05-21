d195a6b07a0be1e470a1be0f959aaad72f4afc82
krishal77 <kthapaliya77@gmail.com>
2026-05-21 21:58:24 +0545

    feat: add togglePublishStatus functionality to toggle video publish state

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -101,5 +101,23 @@ await Video.findByIdAndDelete(videoId)
 //should also delete from cloudinary, will do later
 return res.status(200).json(new ApiResponse(200,{},"Video deleted successfully"))
 })
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid video Id")
+    }
+    const video=await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(404,"No video found")
+    }
 
-export {publishAVideo,updateVideo,deleteVideo}
+   const updatedVideo= await Video.findByIdAndUpdate(videoId,{
+        $set:{
+            isPublished:!video.isPublished
+        }
+    },{
+        new:true
+    })
+    return res.status(200).json(new ApiResponse(200,updatedVideo,"successfully toggled"));
+})
+export {publishAVideo,updateVideo,deleteVideo,togglePublishStatus}
