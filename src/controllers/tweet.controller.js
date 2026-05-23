7286053612065ca9457d41ed0629e8ba3c1096d3
krishal77 <kthapaliya77@gmail.com>
2026-05-23 20:09:37 +0545

    feat: add updatedTweet and deleteTweet controllers for tweet management

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -15,8 +15,65 @@ const createTweet = asyncHandler(async(req,res)=>{
         content,
         owner: req.user?._id 
     })
+    if(!tweet){
+         throw new ApiError(500,"something went wrong while creating tweet")
+    }
 
     return res.status(201).json(new ApiResponse(201,tweet,"tweet is created"))
 })
 
-export {createTweet}
+const updatedTweet = asyncHandler(async (req, res) => {
+    const {content}=req.body
+    const { tweetId } = req.params;
+    if(!content || content.trim() === ""){
+        throw new ApiError(400,"Content is required")
+    }
+     if (!req.user) {
+        throw new ApiError(401, "Unauthorized");
+    }
+
+     const tweet = await Tweet.findById(tweetId);
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You can only update your own tweet");
+    }
+       const updateTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content: content.trim()
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+
+    return res.status(201).json(new ApiResponse(201,updatedTweet,"tweet is updated"))
+})
+const deleteTweet = asyncHandler(async (req, res) => {
+   const{tweetId}=req.params
+  if(!isValidObjectId(tweetId)){
+    throw new ApiError(400, "Invalid tweet id")
+}
+
+const tweet= await Tweet.findById(tweetId)
+if(!tweet){
+    throw new ApiError(404,"tweet is not found")
+}
+if(tweet.owner.toString()!==req.user._id.toString())
+{
+     throw new ApiError(403,"You cant delete this tweet")
+}
+await Tweet.findByIdAndDelete(tweetId)
+
+return res.status(204).json(new ApiResponse(204,{},"tweet is deleted successfully"))
+
+
+})
+export {createTweet,updatedTweet}
