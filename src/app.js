7347c8abd2f3f12a0421cc18271a68e1a9857d7c
krishal77 <kthapaliya77@gmail.com>
2026-05-28 12:23:47 +0545

    feat: add healthcheck route and controller for server status

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import tweetRouter from "./routes/tweet.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 //routes decleration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/video", videoRouter)
@@ -31,4 +32,5 @@ app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter )
 export {app};
